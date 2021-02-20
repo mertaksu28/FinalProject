47767df00a7a7dcbbf43a689906f6576a8ec6d2f
Mert AKSU <mertaksu28@outlook.com.tr>
2021-02-20 20:21:39 +0300

    Core Katmani

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,13 +9,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
             foreach (var product in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(product.ProductName);
             }
-
-
         }
     }
 }
